c9dffac03a1ea4061c733874c40d628b44a37d15
Vikysja <91226103+Vikysja@users.noreply.github.com>
2021-10-20 23:14:27 +0300

    Add files via upload

---
+++ Lab 4.2.cpp
@@ -0,0 +1,52 @@
+﻿// lab_4.2.cpp
+// Квецко Вікторія
+// Лабораторна робота № 4.2
+// Табуляція функції, заданої формулою: функція однієї змінної
+// Варіант 11
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "y" << " |" << endl;
+	cout << "----------------------" << endl;
+
+	x = xp;
+
+	while (x <= xk)
+	{
+		A = 2* abs(5-x);
+
+		if (x <= 1)
+			B = pow(exp(x),abs(2+x));
+		else
+			if (x >= 1)
+				B = pow(cos(x),2)/(1+abs(sin (x)));
+			else
+				B = pow(sin(x),2)*(1/abs(2+x));
+
+		y = A - B;
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+
+		x += dx;
+	}
+
+	cout << "----------------------" << endl;
+
+	return 0;
+}
